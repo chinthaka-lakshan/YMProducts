7e0021446ceb09d56fab9076d623ca4b70ee656a
Yasantha Ravindu <99851400+Ravindu9EV@users.noreply.github.com>
2025-03-07 14:11:38 +0530

    fixed responsive issue and implemented add item form with backend logic

--- YM_Project/src/App.jsx
+++ YM_Project/src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from "react"; // React Hooks
 import reactLogo from "./assets/react.svg"; // Example asset
 import viteLogo from "/vite.svg"; // Example Vite asset
 import "./index.css"; // App-level styles
-
+import "./App";
 import Login from "./pages/Login/login"; // Ensure this path is correct and matches the file name/extension
 import Dashboard from "./pages/dashboard/Dashboard";
 import ItemList from "./components/ItemList/ItemList";
@@ -10,18 +10,28 @@ import TopBar from "./components/TopBar/TopBar";
 import SideBar from "./components/SideBar/SideBar";
 
 function App() {
-    const [count, setCount] = useState(0); // Example state (not currently used in Login)
+  const [count, setCount] = useState(0); // Example state (not currently used in Login)
 
-    return (
-        <div>
-            <TopBar></TopBar>
-            <SideBar></SideBar>
-            {/* <Dashboard></Dashboard> */}
-            <ItemList />
-            {/* <Login />
-       {/* Rendering the Login component */}
+  return (
+    <div>
+      <div className="mainCont">
+        <div className="leftCont">
+          <SideBar></SideBar>
         </div>
-    );
+        <div className="rightCont">
+          <TopBar></TopBar>
+          <div className="routeContent">
+            {/* <Dashboard></Dashboard> */}
+            <ItemList></ItemList>
+            </div>
+        </div>
+      </div>
+
+      {/* <ItemList /> */}
+      {/* <Login />
+       {/* Rendering the Login component */}
+    </div>
+  );
 }
 
 export default App;
